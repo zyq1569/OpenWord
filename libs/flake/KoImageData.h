d2c4543d188fbddd30eb7e1692014956c966c578
zyq1569 <zyq1569@126.com>
2021-01-23 21:47:13 +0800

    libs/flake/KoImageData.h libs/flake/KoImageData_p.h

--- libs/flake/KoImageData.h
+++ libs/flake/KoImageData.h
@@ -25,7 +25,11 @@
 
 #include <QSize>
 #include <QMetaType>
-
+#include <QTimer>
+#include <QDir>
+#include <QUrl>
+#include <QImage>
+#include <QPixmap>
 #include <KoShapeUserData.h>
 
 class QIODevice;
@@ -135,5 +139,63 @@ private:
 };
 
 Q_DECLARE_METATYPE(KoImageData*)
+
+
+class KoImageCollection;
+class QTemporaryFile;
+
+class KoImageDataPrivate
+{
+public:
+    explicit KoImageDataPrivate(KoImageData *q);
+    virtual ~KoImageDataPrivate();
+
+    /**
+     * Save the image data to the param device.
+     * The full file is saved.
+     * @param device the device that is used to get the data from.
+     * @return returns true if load was successful.
+     */
+    bool saveData(QIODevice &device);
+
+    /// store the suffix based on the full filename.
+    void setSuffix(const QString &fileName);
+
+    /// take the data from \a device and store it in the temporaryFile
+    void copyToTemporary(QIODevice &device);
+
+    /// clean the image cache.
+    void cleanupImageCache();
+
+    void clear();
+
+    static qint64 generateKey(const QByteArray &bytes);
+
+    enum DataStoreState {
+        StateEmpty,     ///< No image data, either as url or as QImage
+        StateNotLoaded, ///< Image data is set as Url
+        StateImageLoaded,///< Image data is loaded from Url, so both are present.
+        StateImageOnly  ///< Image data is stored in a QImage. There is no external storage.
+    };
+
+    KoImageCollection *collection;
+    KoImageData::ErrorCode errorCode;
+    QSizeF imageSize;
+    qint64 key;
+    QString suffix; // the suffix of the picture e.g. png  TODO use a QByteArray ?
+    QTimer cleanCacheTimer;
+
+    QAtomicInt refCount;
+
+    // Image data store.
+    DataStoreState dataStoreState;
+    QUrl imageLocation;
+    QImage image;
+    /// screen optimized cached version.
+    QPixmap pixmap;
+
+    QTemporaryFile *temporaryFile;
+};
+
 
 #endif
--- libs/flake/KoImageData_p.h
+++ libs/flake/KoImageData_p.h
@@ -23,69 +23,69 @@
 #ifndef KOIMAGEDATA_P_H
 #define KOIMAGEDATA_P_H
 
-#include <QUrl>
-#include <QByteArray>
-#include <QImage>
-#include <QPixmap>
-#include <QTimer>
-#include <QDir>
+//#include <QUrl>
+//#include <QByteArray>
+//#include <QImage>
+//#include <QPixmap>
+//#include <QTimer>
+//#include <QDir>
 
-#include "KoImageData.h"
+//#include "KoImageData.h"
 
-class KoImageCollection;
-class QTemporaryFile;
+//class KoImageCollection;
+//class QTemporaryFile;
 
-class KoImageDataPrivate
-{
-public:
-    explicit KoImageDataPrivate(KoImageData *q);
-    virtual ~KoImageDataPrivate();
+//class KoImageDataPrivate
+//{
+//public:
+//    explicit KoImageDataPrivate(KoImageData *q);
+//    virtual ~KoImageDataPrivate();
 
-    /**
-     * Save the image data to the param device.
-     * The full file is saved.
-     * @param device the device that is used to get the data from.
-     * @return returns true if load was successful.
-     */
-    bool saveData(QIODevice &device);
+//    /**
+//     * Save the image data to the param device.
+//     * The full file is saved.
+//     * @param device the device that is used to get the data from.
+//     * @return returns true if load was successful.
+//     */
+//    bool saveData(QIODevice &device);
 
-    /// store the suffix based on the full filename.
-    void setSuffix(const QString &fileName);
+//    /// store the suffix based on the full filename.
+//    void setSuffix(const QString &fileName);
 
-    /// take the data from \a device and store it in the temporaryFile
-    void copyToTemporary(QIODevice &device);
+//    /// take the data from \a device and store it in the temporaryFile
+//    void copyToTemporary(QIODevice &device);
 
-    /// clean the image cache.
-    void cleanupImageCache();
+//    /// clean the image cache.
+//    void cleanupImageCache();
 
-    void clear();
+//    void clear();
 
-    static qint64 generateKey(const QByteArray &bytes);
+//    static qint64 generateKey(const QByteArray &bytes);
 
-    enum DataStoreState {
-        StateEmpty,     ///< No image data, either as url or as QImage
-        StateNotLoaded, ///< Image data is set as Url
-        StateImageLoaded,///< Image data is loaded from Url, so both are present.
-        StateImageOnly  ///< Image data is stored in a QImage. There is no external storage.
-    };
+//    enum DataStoreState {
+//        StateEmpty,     ///< No image data, either as url or as QImage
+//        StateNotLoaded, ///< Image data is set as Url
+//        StateImageLoaded,///< Image data is loaded from Url, so both are present.
+//        StateImageOnly  ///< Image data is stored in a QImage. There is no external storage.
+//    };
 
-    KoImageCollection *collection;
-    KoImageData::ErrorCode errorCode;
-    QSizeF imageSize;
-    qint64 key;
-    QString suffix; // the suffix of the picture e.g. png  TODO use a QByteArray ?
-    QTimer cleanCacheTimer;
+//    KoImageCollection *collection;
+//    KoImageData::ErrorCode errorCode;
+//    QSizeF imageSize;
+//    qint64 key;
+//    QString suffix; // the suffix of the picture e.g. png  TODO use a QByteArray ?
+//    QTimer cleanCacheTimer;
 
-    QAtomicInt refCount;
+//    QAtomicInt refCount;
 
-    // Image data store.
-    DataStoreState dataStoreState;
-    QUrl imageLocation;
-    QImage image;
-    /// screen optimized cached version.
-    QPixmap pixmap;
+//    // Image data store.
+//    DataStoreState dataStoreState;
+//    QUrl imageLocation;
+//    QImage image;
+//    /// screen optimized cached version.
+//    QPixmap pixmap;
 
-    QTemporaryFile *temporaryFile;
-};
+//    QTemporaryFile *temporaryFile;
+//};
 
 #endif /* KOIMAGEDATA_P_H */
