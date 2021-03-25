8d3a195312dbf3d83602bc976824441b6461c74e
zyq1569 <zyq1569@126.com>
2021-03-25 19:53:59 +0800

    Modified   libs/basicflakes/tools/KoPencilTool.cpp

--- libs/basicflakes/tools/KoPencilTool.cpp
+++ libs/basicflakes/tools/KoPencilTool.cpp
@@ -52,10 +52,10 @@
 #include "KoCreatePathTool_p.h"
 
 KoPencilTool::KoPencilTool(KoCanvasBase *canvas)
-        : KoToolBase(canvas),  m_mode(ModeCurve), m_optimizeRaw(false)
-        , m_optimizeCurve(false), m_combineAngle(15.0), m_fittingError(5.0)
-        , m_close(false), m_shape(0)
-        , m_existingStartPoint(0), m_existingEndPoint(0), m_hoveredPoint(0)
+    : KoToolBase(canvas),  m_mode(ModeCurve), m_optimizeRaw(false)
+    , m_optimizeCurve(false), m_combineAngle(15.0), m_fittingError(5.0)
+    , m_close(false), m_shape(0)
+    , m_existingStartPoint(0), m_existingEndPoint(0), m_hoveredPoint(0)
 {
 }
 
@@ -65,7 +65,8 @@ KoPencilTool::~KoPencilTool()
 
 void KoPencilTool::paint(QPainter &painter, const KoViewConverter &converter)
 {
-    if (m_shape) {
+    if (m_shape)
+    {
         painter.save();
 
         painter.setTransform(m_shape->absoluteTransformation(&converter) * painter.transform());
@@ -75,7 +76,8 @@ void KoPencilTool::paint(QPainter &painter, const KoViewConverter &converter)
         m_shape->paint(painter, converter, paintContext);
         painter.restore();
 
-        if (m_shape->stroke()) {
+        if (m_shape->stroke())
+        {
             painter.save();
             m_shape->stroke()->paint(m_shape, painter, converter);
             painter.restore();
@@ -84,7 +86,8 @@ void KoPencilTool::paint(QPainter &painter, const KoViewConverter &converter)
         painter.restore();
     }
 
-    if (m_hoveredPoint) {
+    if (m_hoveredPoint)
+    {
         painter.save();
         painter.setTransform(m_hoveredPoint->parent()->absoluteTransformation(&converter), true);
         KoShape::applyConversion(painter, converter);
@@ -103,7 +106,8 @@ void KoPencilTool::repaintDecorations()
 
 void KoPencilTool::mousePressEvent(KoPointerEvent *event)
 {
-    if (! m_shape) {
+    if (! m_shape)
+    {
         m_shape = new KoPathShape();
         m_shape->setShapeId(KoPathShapeId);
         m_shape->setStroke(createStroke());
@@ -112,7 +116,9 @@ void KoPencilTool::mousePressEvent(KoPointerEvent *event)
         QPointF point = event->point;
         m_existingStartPoint = endPointAtPosition(point);
         if (m_existingStartPoint)
+        {
             point = m_existingStartPoint->parent()->shapeToDocument(m_existingStartPoint->point());
+        }
 
         addPoint(point);
     }
@@ -121,16 +127,21 @@ void KoPencilTool::mousePressEvent(KoPointerEvent *event)
 void KoPencilTool::mouseMoveEvent(KoPointerEvent *event)
 {
     if (event->buttons() & Qt::LeftButton)
+    {
         addPoint(event->point);
+    }
 
     KoPathPoint * endPoint = endPointAtPosition(event->point);
-    if (m_hoveredPoint != endPoint) {
-        if (m_hoveredPoint) {
+    if (m_hoveredPoint != endPoint)
+    {
+        if (m_hoveredPoint)
+        {
             QPointF nodePos = m_hoveredPoint->parent()->shapeToDocument(m_hoveredPoint->point());
             canvas()->updateCanvas(handlePaintRect(nodePos));
         }
         m_hoveredPoint = endPoint;
-        if (m_hoveredPoint) {
+        if (m_hoveredPoint)
+        {
             QPointF nodePos = m_hoveredPoint->parent()->shapeToDocument(m_hoveredPoint->point());
             canvas()->updateCanvas(handlePaintRect(nodePos));
         }
@@ -140,12 +151,16 @@ void KoPencilTool::mouseMoveEvent(KoPointerEvent *event)
 void KoPencilTool::mouseReleaseEvent(KoPointerEvent *event)
 {
     if (! m_shape)
+    {
         return;
+    }
 
     QPointF point = event->point;
     m_existingEndPoint = endPointAtPosition(point);
     if (m_existingEndPoint)
+    {
         point = m_existingEndPoint->parent()->shapeToDocument(m_existingEndPoint->point());
+    }
 
     addPoint(point);
     finish(event->modifiers() & Qt::ShiftModifier);
@@ -163,9 +178,12 @@ void KoPencilTool::mouseReleaseEvent(KoPointerEvent *event)
 
 void KoPencilTool::keyPressEvent(QKeyEvent *event)
 {
-    if (m_shape) {
+    if (m_shape)
+    {
         event->accept();
-    } else {
+    }
+    else
+    {
         event->ignore();
     }
 }
@@ -190,16 +208,24 @@ void KoPencilTool::deactivate()
 void KoPencilTool::addPoint(const QPointF & point)
 {
     if (! m_shape)
+    {
         return;
+    }
 
     // do a moveTo for the first point added
     if (m_points.empty())
+    {
         m_shape->moveTo(point);
+    }
     // do not allow coincident points
     else if (point != m_points.last())
+    {
         m_shape->lineTo(point);
+    }
     else
+    {
         return;
+    }
 
     m_points.append(point);
     canvas()->updateCanvas(m_shape->boundingRect());
@@ -209,7 +235,9 @@ qreal KoPencilTool::lineAngle(const QPointF &p1, const QPointF &p2)
 {
     qreal angle = atan2(p2.y() - p1.y(), p2.x() - p1.x());
     if (angle < 0.0)
+    {
         angle += 2 * M_PI;
+    }
 
     return angle * 180.0 / M_PI;
 }
@@ -217,19 +245,26 @@ qreal KoPencilTool::lineAngle(const QPointF &p1, const QPointF &p2)
 void KoPencilTool::finish(bool closePath)
 {
     if (m_points.count() < 2)
+    {
         return;
+    }
 
     KoPathShape * path = 0;
     QVector<QPointF> complete;
     QVector<QPointF> *points = &m_points;
 
-    if (m_mode == ModeStraight || m_optimizeRaw || m_optimizeCurve) {
+    if (m_mode == ModeStraight || m_optimizeRaw || m_optimizeCurve)
+    {
         float combineAngle;
 
         if (m_mode == ModeStraight)
+        {
             combineAngle = m_combineAngle;
+        }
         else
+        {
             combineAngle = 0.50f;
+        }
 
         //Add the first two points
         complete.append(m_points[0]);
@@ -239,10 +274,13 @@ void KoPencilTool::finish(bool closePath)
         float lastAngle = lineAngle(complete[0], complete[1]);
 
         uint pointCount = m_points.count();
-        for (uint i = 2; i < pointCount; ++i) {
+        for (uint i = 2; i < pointCount; ++i)
+        {
             float angle = lineAngle(complete.last(), m_points[i]);
             if (qAbs(angle - lastAngle) < combineAngle)
+            {
                 complete.removeLast();
+            }
             complete.append(m_points[i]);
             lastAngle = angle;
         }
@@ -251,24 +289,31 @@ void KoPencilTool::finish(bool closePath)
         points = &complete;
     }
 
-    switch (m_mode) {
-    case ModeCurve: {
-        path = bezierFit(*points, m_fittingError);
-    }
-    break;
-    case ModeStraight:
-    case ModeRaw: {
-        path = new KoPathShape();
-        uint pointCount = points->count();
-        path->moveTo(points->at(0));
-        for (uint i = 1; i < pointCount; ++i)
-            path->lineTo(points->at(i));
-    }
-    break;
+    switch (m_mode)
+    {
+        case ModeCurve:
+        {
+            path = bezierFit(*points, m_fittingError);
+        }
+        break;
+        case ModeStraight:
+        case ModeRaw:
+        {
+            path = new KoPathShape();
+            uint pointCount = points->count();
+            path->moveTo(points->at(0));
+            for (uint i = 1; i < pointCount; ++i)
+            {
+                path->lineTo(points->at(i));
+            }
+        }
+        break;
     }
 
     if (! path)
+    {
         return;
+    }
 
     path->setShapeId(KoPathShapeId);
     path->setStroke(createStroke());
@@ -357,32 +402,47 @@ void KoPencilTool::addPathShape(KoPathShape* path, bool closePath)
     KoShape * startShape = 0;
     KoShape * endShape = 0;
 
-    if (closePath) {
+    if (closePath)
+    {
         path->close();
         path->normalize();
-    } else {
+    }
+    else
+    {
         path->normalize();
-        if (connectPaths(path, m_existingStartPoint, m_existingEndPoint)) {
+        if (connectPaths(path, m_existingStartPoint, m_existingEndPoint))
+        {
             if (m_existingStartPoint)
+            {
                 startShape = m_existingStartPoint->parent();
+            }
             if (m_existingEndPoint && m_existingEndPoint != m_existingStartPoint)
+            {
                 endShape = m_existingEndPoint->parent();
+            }
         }
     }
 
     KUndo2Command * cmd = canvas()->shapeController()->addShape(path);
-    if (cmd) {
+    if (cmd)
+    {
         KoSelection *selection = canvas()->shapeManager()->selection();
         selection->deselectAll();
         selection->select(path);
 
         if (startShape)
+        {
             canvas()->shapeController()->removeShape(startShape, cmd);
+        }
         if (endShape && startShape != endShape)
+        {
             canvas()->shapeController()->removeShape(endShape, cmd);
+        }
 
         canvas()->addCommand(cmd);
-    } else {
+    }
+    else
+    {
         canvas()->updateCanvas(path->boundingRect());
         delete path;
     }
@@ -396,23 +456,32 @@ void KoPencilTool::selectMode(int mode)
 void KoPencilTool::setOptimize(int state)
 {
     if (m_mode == ModeRaw)
+    {
         m_optimizeRaw = state == Qt::Checked ? true : false;
+    }
     else
+    {
         m_optimizeCurve = state == Qt::Checked ? true : false;
+    }
 }
 
 void KoPencilTool::setDelta(double delta)
 {
     if (m_mode == ModeCurve)
+    {
         m_fittingError = delta;
+    }
     else if (m_mode == ModeStraight)
+    {
         m_combineAngle = delta;
+    }
 }
 
 KoShapeStroke* KoPencilTool::createStroke()
 {
     KoShapeStroke *stroke = 0;
-    if (m_strokeWidget) {
+    if (m_strokeWidget)
+    {
         stroke = m_strokeWidget->createShapeStroke();
     }
     return stroke;
@@ -427,30 +496,40 @@ KoPathPoint* KoPencilTool::endPointAtPosition(const QPointF &position)
     qreal minDistance = HUGE_VAL;
     qreal maxDistance = canvas()->viewConverter()->viewToDocumentX(grabSensitivity());
 
-    foreach(KoShape *shape, shapes) {
+    foreach(KoShape *shape, shapes)
+    {
         KoPathShape * path = dynamic_cast<KoPathShape*>(shape);
         if (!path)
+        {
             continue;
+        }
         KoParameterShape *paramShape = dynamic_cast<KoParameterShape*>(shape);
         if (paramShape && paramShape->isParametricShape())
+        {
             continue;
+        }
 
         KoPathPoint * p = 0;
         uint subpathCount = path->subpathCount();
-        for (uint i = 0; i < subpathCount; ++i) {
+        for (uint i = 0; i < subpathCount; ++i)
+        {
             if (path->isClosedSubpath(i))
+            {
                 continue;
+            }
             p = path->pointByIndex(KoPathPointIndex(i, 0));
             // check start of subpath
             qreal d = squareDistance(position, path->shapeToDocument(p->point()));
-            if (d < minDistance && d < maxDistance) {
+            if (d < minDistance && d < maxDistance)
+            {
                 nearestPoint = p;
                 minDistance = d;
             }
             // check end of subpath
             p = path->pointByIndex(KoPathPointIndex(i, path->subpathPointCount(i) - 1));
             d = squareDistance(position, path->shapeToDocument(p->point()));
-            if (d < minDistance && d < maxDistance) {
+            if (d < minDistance && d < maxDistance)
+            {
                 nearestPoint = p;
                 minDistance = d;
             }
@@ -464,10 +543,14 @@ bool KoPencilTool::connectPaths(KoPathShape *pathShape, KoPathPoint *pointAtStar
 {
     // at least one point must be valid
     if (!pointAtStart && !pointAtEnd)
+    {
         return false;
+    }
     // do not allow connecting to the same point twice
     if (pointAtStart == pointAtEnd)
+    {
         pointAtEnd = 0;
+    }
 
     // we have hit an existing path point on start/finish
     // what we now do is:
@@ -485,16 +568,19 @@ bool KoPencilTool::connectPaths(KoPathShape *pathShape, KoPathPoint *pointAtStar
 
     // combine with the path we hit on start
     KoPathPointIndex startIndex(-1, -1);
-    if (pointAtStart) {
+    if (pointAtStart)
+    {
         startIndex = startShape->pathPointIndex(pointAtStart);
         pathShape->combine(startShape);
         pathShape->moveSubpath(0, pathShape->subpathCount() - 1);
     }
     // combine with the path we hit on finish
     KoPathPointIndex endIndex(-1, -1);
-    if (pointAtEnd) {
+    if (pointAtEnd)
+    {
         endIndex = endShape->pathPointIndex(pointAtEnd);
-        if (endShape != startShape) {
+        if (endShape != startShape)
+        {
             endIndex.first += pathShape->subpathCount();
             pathShape->combine(endShape);
         }
@@ -502,11 +588,13 @@ bool KoPencilTool::connectPaths(KoPathShape *pathShape, KoPathPoint *pointAtStar
     // do we connect twice to a single subpath ?
     bool connectToSingleSubpath = (startShape == endShape && startIndex.first == endIndex.first);
 
-    if (startIndex.second == 0 && !connectToSingleSubpath) {
+    if (startIndex.second == 0 && !connectToSingleSubpath)
+    {
         pathShape->reverseSubpath(startIndex.first);
         startIndex.second = pathShape->subpathPointCount(startIndex.first) - 1;
     }
-    if (endIndex.second > 0 && !connectToSingleSubpath) {
+    if (endIndex.second > 0 && !connectToSingleSubpath)
+    {
         pathShape->reverseSubpath(endIndex.first);
         endIndex.second = 0;
     }
@@ -523,14 +611,16 @@ bool KoPencilTool::connectPaths(KoPathShape *pathShape, KoPathPoint *pointAtStar
     KoPathPoint * existingEndPoint = pathShape->pointByIndex(endIndex);
 
     // merge first two points
-    if (existingStartPoint) {
+    if (existingStartPoint)
+    {
         KoPathPointData pd1(pathShape, pathShape->pathPointIndex(existingStartPoint));
         KoPathPointData pd2(pathShape, pathShape->pathPointIndex(newStartPoint));
         KoPathPointMergeCommand cmd1(pd1, pd2);
         cmd1.redo();
     }
     // merge last two points
-    if (existingEndPoint) {
+    if (existingEndPoint)
+    {
         KoPathPointData pd3(pathShape, pathShape->pathPointIndex(newEndPoint));
         KoPathPointData pd4(pathShape, pathShape->pathPointIndex(existingEndPoint));
         KoPathPointMergeCommand cmd2(pd3, pd4);
@@ -540,10 +630,12 @@ bool KoPencilTool::connectPaths(KoPathShape *pathShape, KoPathPoint *pointAtStar
     return true;
 }
 
-qreal KoPencilTool::getFittingError(){
+qreal KoPencilTool::getFittingError()
+{
     return this->m_fittingError;
 }
 
-void KoPencilTool::setFittingError(qreal fittingError){
+void KoPencilTool::setFittingError(qreal fittingError)
+{
     this->m_fittingError = fittingError;
 }
