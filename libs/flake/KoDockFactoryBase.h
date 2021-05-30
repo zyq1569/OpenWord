1c2a51fc54965bafdcc86f663389bd2333084fb5
zyq1569 <zyq1569@126.com>
2021-05-30 09:14:13 +0800

    Modified   libs/flake/KoDockFactoryBase.h

--- libs/flake/KoDockFactoryBase.h
+++ libs/flake/KoDockFactoryBase.h
@@ -33,7 +33,8 @@ class QString;
 class FLAKE_EXPORT KoDockFactoryBase
 {
 public:
-    enum DockPosition {
+    enum DockPosition
+    {
         DockTornOff, ///< Floating as its own top level window
         DockTop,    ///< Above the central widget
         DockBottom, ///< Below the central widget
@@ -53,11 +54,13 @@ public:
 
     /// @return the dockers default visibility
     /// Default is true.
-    virtual bool defaultVisible() const {
+    virtual bool defaultVisible() const
+    {
         return true;
     }
     /// Returns true if the dock widget should get a collapsable header.
-    virtual bool isCollapsable() const {
+    virtual bool isCollapsable() const
+    {
         return true;
     }
 
@@ -65,7 +68,8 @@ public:
      * In case the docker is collapsable, returns true if the dock widget
      * will start collapsed by default.
      */
-    virtual bool defaultCollapsed() const {
+    virtual bool defaultCollapsed() const
+    {
         return false;
     }
 
