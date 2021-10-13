1d4938967f5e66f1b2582241387ae58f241e2d67
zyq1569 <zyq1569@126.com>
2021-10-13 21:09:04 +0800

    Modified   filters/words/docx/import/DocxXmlNumberingReader.cpp

--- filters/words/docx/import/DocxXmlNumberingReader.cpp
+++ filters/words/docx/import/DocxXmlNumberingReader.cpp
@@ -421,6 +421,11 @@ KoFilter::ConversionStatus DocxXmlNumberingReader::read_numFmt()
             m_currentBulletProperties.setNumFormat("1");
             m_currentBulletProperties.setSuffix(".");
         }
+        else if (val == "none") //add openword
+        {
+            // <w:numFmt w:val="none"/> --->?"decimal" openword 20211012
+            //m_currentBulletProperties.setNumFormat("1");
+        }
     }
 
     readNext();
