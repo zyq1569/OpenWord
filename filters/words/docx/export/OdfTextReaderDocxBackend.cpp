1fc47bead5d3d4e17728e3461857db629718d62a
zyq1569 <zyq1569@126.com>
2021-12-29 16:22:09 +0800

    Modified   filters/words/docx/export/OdfTextReaderDocxBackend.cpp

--- filters/words/docx/export/OdfTextReaderDocxBackend.cpp
+++ filters/words/docx/export/OdfTextReaderDocxBackend.cpp
@@ -59,6 +59,7 @@ OdfTextReaderDocxBackend::OdfTextReaderDocxBackend()
     , m_insideComment(false)
     , m_insideDcCreator(false)
     , m_insideDcDate(false)
+    , m_insidetable(false)
     , m_currentParagraphTextProperties(0)
 {
 }
@@ -154,6 +155,10 @@ void OdfTextReaderDocxBackend::elementTextP(KoXmlStreamReader &reader, OdfReader
     if (reader.isStartElement())
     {
         writer->startElement("w:p");
+        if (m_insidetable)
+        {
+            return;
+        }
         // FIXME: Add paragraph attributes here
         writer->startElement("w:pPr");
         if (m_currentOutlineLevel >= 0)
@@ -176,8 +181,7 @@ void OdfTextReaderDocxBackend::elementTextP(KoXmlStreamReader &reader, OdfReader
                 if (m_currentParagraphParent.contains("_20"))
                 {
                     QString stylenamenew = m_currentParagraphParent;
-                    stylenamenew.replace(QLatin1String("_20"), QLatin1String(""));
-                    writer->addAttribute("w:val", stylenamenew);
+                    writer->addAttribute("w:val", stylenamenew.replace(QLatin1String("_20"), QLatin1String("")));
                 }
                 else
                 {
@@ -199,6 +203,303 @@ void OdfTextReaderDocxBackend::elementTextP(KoXmlStreamReader &reader, OdfReader
     }
 }
 
+
+///---------------------------- TableTable
+void OdfTextReaderDocxBackend::elementTableTable(KoXmlStreamReader &reader, OdfReaderContext *context)
+{
+    DEBUG_BACKEND();
+    OdfReaderDocxContext *docxContext = dynamic_cast<OdfReaderDocxContext*>(context);
+    if (!docxContext)
+    {
+        return;
+    }
+    KoXmlWriter *writer = docxContext->m_documentWriter;
+    if (m_insideComment)
+    {
+        writer = docxContext->m_commentsWriter;
+    }
+    if (reader.isStartElement())
+    {
+        m_insidetable = true;
+        writer->startElement("w:tbl");
+
+        writer->startElement("w:tblPr");
+        // eg:
+        // <w:tblW w:w="5000" w:type="pct"/>
+        // <w:tblInd w:w="100" w:type="dxa"/>
+        // <w:tblCellMar>
+        //     <w:left w:w="10" w:type="dxa"/>
+        //     <w:right w:w="10" w:type="dxa"/>
+        // </w:tblCellMar>
+        // <w:tblLook w:val="0000" w:firstRow="0" w:lastRow="0" w:firstColumn="0" w:lastColumn="0" w:noHBand="0" w:noVBand="0"/>
+        writer->startElement("w:tblW");
+        writer->addAttribute("w:w","5000");
+        writer->addAttribute("w:type","pct");
+        writer->endElement();//w:tblW
+
+        writer->startElement("w:tblInd");
+        writer->addAttribute("w:w","100");
+        writer->addAttribute("w:type","dxa");
+        writer->endElement();//w:tblInd
+
+        writer->startElement("w:tblCellMar");
+        writer->startElement("w:left");
+        writer->addAttribute("w:w","10");
+        writer->addAttribute("w:type","dxa");
+        writer->endElement();//w:left
+        writer->startElement("w:right");
+        writer->addAttribute("w:w","10");
+        writer->addAttribute("w:type","dxa");
+        writer->endElement();//w:right
+        writer->endElement();//w:tblCellMar
+
+        writer->startElement("w:tblLook");
+        writer->addAttribute("w:val","0000");
+        writer->addAttribute("w:firstRow","0");
+        writer->addAttribute("w:lastRow","0");
+        writer->addAttribute("w:firstColumn","0");
+        writer->addAttribute("w:lastColumn","0");
+        writer->addAttribute("w:noHBand","0");
+        writer->addAttribute("w:noVBand","0");
+        writer->endElement();//w:tblLook
+        writer->endElement(); // w:tblPr
+
+    }
+    else
+    {
+        writer->endElement();//"w:tbl"
+        m_insidetable = false;
+    }
+
+}
+
+/// odf : eg <table:table-column table:style-name="Table1.A" table:number-columns-repeated="4"/>
+void OdfTextReaderDocxBackend::elementTableTableColumn(KoXmlStreamReader &reader, OdfReaderContext *context)
+{
+    DEBUG_BACKEND();
+    OdfReaderDocxContext *docxContext = dynamic_cast<OdfReaderDocxContext*>(context);
+    if (!docxContext)
+    {
+        return;
+    }
+    KoXmlWriter *writer = docxContext->m_documentWriter;
+    if (m_insideComment)
+    {
+        writer = docxContext->m_commentsWriter;
+    }
+    if (reader.isStartElement())
+    {
+        writer->startElement("w:tblGrid");
+        ///------------------------------
+        KoXmlStreamAttributes attributes = reader.attributes();
+        int number_columns = attributes.value("table:number-columns-repeated").toString().toInt();
+        writer->startElement("w:gridCol");
+        writer->addAttribute("w:w","2110");
+        writer->endElement();//w:gridCol
+        for (int i=1; i<number_columns; i++)
+        {
+            writer->startElement("w:gridCol");
+            writer->addAttribute("w:w","2490");
+            writer->endElement();//w:gridCol
+        }
+    }
+    else
+    {
+        writer->endElement();//"w:tblGrid"
+    }
+}
+
+
+void OdfTextReaderDocxBackend::elementTableTableColumns(KoXmlStreamReader &reader, OdfReaderContext *context)
+{
+    DEBUG_BACKEND();
+    OdfReaderDocxContext *docxContext = dynamic_cast<OdfReaderDocxContext*>(context);
+    if (!docxContext)
+    {
+        return;
+    }
+    KoXmlWriter *writer = docxContext->m_documentWriter;
+    if (m_insideComment)
+    {
+        writer = docxContext->m_commentsWriter;
+    }
+    // docx eg:
+    //<w:tblGrid>
+    //    <w:gridCol w:w="2110"/>
+    //    <w:gridCol w:w="2490"/>
+    //    <w:gridCol w:w="2490"/>
+    //    <w:gridCol w:w="2490"/>
+    //</w:tblGrid>
+    if (reader.isStartElement())
+    {
+        writer->startElement("w:tblGrid");
+        KoXmlStreamAttributes attributes = reader.attributes();
+        int number_columns = attributes.value("table:number-columns-repeated").toString().toInt();
+        writer->startElement("w:gridCol");
+        writer->addAttribute("w:w","2110");
+        writer->endElement();//w:gridCol
+        for (int i=1; i<number_columns; i++)
+        {
+            writer->startElement("w:gridCol");
+            writer->addAttribute("w:w","2490");
+            writer->endElement();//w:gridCol
+        }
+    }
+    else
+    {
+        writer->endElement();//"w:tblGrid"
+    }
+}
+
+
+void OdfTextReaderDocxBackend::elementTableTableRow(KoXmlStreamReader &reader, OdfReaderContext *context)
+{
+    DEBUG_BACKEND();
+    OdfReaderDocxContext *docxContext = dynamic_cast<OdfReaderDocxContext*>(context);
+    if (!docxContext)
+    {
+        return;
+    }
+    KoXmlWriter *writer = docxContext->m_documentWriter;
+    if (m_insideComment)
+    {
+        writer = docxContext->m_commentsWriter;
+    }
+    // docx eg:
+    //<w:tr
+    //  <w:tblPrEx>
+    //      <w:tblCellMar>
+    //          <w:top w:w="0" w:type="dxa"/>
+    //          <w:bottom w:w="0" w:type="dxa"/>
+    //      </w:tblCellMar>
+    //  </w:tblPrEx>
+    //...
+    //</w:tr>
+    if (reader.isStartElement())
+    {
+        writer->startElement("w:tr");
+
+        writer->startElement("w:tblPrEx");
+        writer->startElement("w:tblCellMar");
+        writer->startElement("w:top");
+        writer->addAttribute("w:w","0");
+        writer->addAttribute("w:type","dxa");
+        writer->endElement();//w:top
+        writer->startElement("w:bottom");
+        writer->addAttribute("w:w","0");
+        writer->addAttribute("w:type","dxa");
+        writer->endElement();//w:bottom
+        writer->endElement();// <w:tblCellMar>
+        writer->endElement();//<w:tblPrEx>
+    }
+    else
+    {
+        writer->endElement();//"w:tr"
+    }
+}
+
+
+void OdfTextReaderDocxBackend::elementTableTableCell(KoXmlStreamReader &reader, OdfReaderContext *context)
+{
+    DEBUG_BACKEND();
+    OdfReaderDocxContext *docxContext = dynamic_cast<OdfReaderDocxContext*>(context);
+    if (!docxContext)
+    {
+        return;
+    }
+    KoXmlWriter *writer = docxContext->m_documentWriter;
+    if (m_insideComment)
+    {
+        writer = docxContext->m_commentsWriter;
+    }
+    //<w:tc>
+    //    <w:tcPr>
+    //        <w:tcW w:w="0" w:type="auto"/>
+    //        <w:tcBorders>
+    //            <w:top w:val="single" w:sz="18" w:space="0" w:color="000000"/>
+    //            <w:left w:val="single" w:sz="18" w:space="0" w:color="000000"/>
+    //            <w:bottom w:val="single" w:sz="18" w:space="0" w:color="000000"/>
+    //            <w:right w:val="single" w:sz="18" w:space="0" w:color="000000"/>
+    //        </w:tcBorders>
+    //        <w:tcMar>
+    //            <w:top w:w="100" w:type="dxa"/>
+    //            <w:left w:w="100" w:type="dxa"/>
+    //            <w:bottom w:w="100" w:type="dxa"/>
+    //            <w:right w:w="100" w:type="dxa"/>
+    //        </w:tcMar>
+    //    </w:tcPr>
+    //    <w:p w14:paraId="6F0111A3" w14:textId="77777777" w:rsidR="000B243E" w:rsidRDefault="00A21553">
+    //        <w:r>
+    //            <w:t>序号</w:t>
+    //        </w:r>
+    //    </w:p>
+    //</w:tc>
+
+    if (reader.isStartElement())
+    {
+        writer->startElement("w:tc");
+
+        writer->startElement("w:tcPr");
+
+        writer->startElement("w:tcW");
+        writer->addAttribute("w:w","0");
+        writer->addAttribute("w:type","auto");
+        writer->endElement();//w:tcW
+
+        writer->startElement("w:tcBorders");
+        writer->startElement("w:top");
+        writer->addAttribute("w:val","single");
+        writer->addAttribute("w:sz","18");
+        writer->addAttribute("w:space","0");
+        writer->addAttribute("w:color","000000");
+        writer->endElement();//w:top
+        writer->startElement("w:left");
+        writer->addAttribute("w:val","single");
+        writer->addAttribute("w:sz","18");
+        writer->addAttribute("w:space","0");
+        writer->addAttribute("w:color","000000");
+        writer->endElement();//w:left
+        writer->startElement("w:bottom");
+        writer->addAttribute("w:val","single");
+        writer->addAttribute("w:sz","18");
+        writer->addAttribute("w:space","0");
+        writer->addAttribute("w:color","000000");
+        writer->endElement();//w:bottom
+        writer->startElement("w:right");
+        writer->addAttribute("w:val","single");
+        writer->addAttribute("w:sz","18");
+        writer->addAttribute("w:space","0");
+        writer->addAttribute("w:color","000000");
+        writer->endElement();//w:right
+        writer->endElement();//w:tcBorders
+
+        writer->startElement("w:tcMar");
+        writer->startElement("w:top");
+        writer->addAttribute("w:w","100");
+        writer->addAttribute("w:type","dxa");
+        writer->endElement();//w:top
+        writer->startElement("w:left");
+        writer->addAttribute("w:w","100");
+        writer->addAttribute("w:type","dxa");
+        writer->endElement();//w:left
+        writer->startElement("w:bottom");
+        writer->addAttribute("w:w","100");
+        writer->addAttribute("w:type","dxa");
+        writer->endElement();//w:bottom
+        writer->startElement("w:right");
+        writer->addAttribute("w:w","100");
+        writer->addAttribute("w:type","dxa");
+        writer->endElement();//w:right
+        writer->endElement();//w:tcMar
+
+        writer->endElement();//<w:tcPr>
+
+    }
+    else
+    {
+        writer->endElement();//"w:tc"
+    }
+}
 
 // ----------------------------------------------------------------
 // Paragraph level functions: spans, annotations, notes, text content itself, etc.
@@ -320,8 +621,11 @@ void OdfTextReaderDocxBackend::startRun(const KoXmlStreamReader &reader, OdfRead
         writer->addAttribute("w:id", m_commentIndex);
         writer->endElement(); // w:commentRangeStart
     }
-    writer->startElement("w:r");
-    writer->startElement("w:rPr");
+    if (!m_insidetable)
+    {
+        writer->startElement("w:r");
+        writer->startElement("w:rPr");
+    }
     KoXmlStreamAttributes attributes = reader.attributes();
     KoOdfStyleProperties properties;
     if (!m_currentParagraphParent.isEmpty())
@@ -351,8 +655,10 @@ void OdfTextReaderDocxBackend::startRun(const KoXmlStreamReader &reader, OdfRead
         }
     }
     DocxStyleHelper::handleTextStyles(&properties, writer);
-
-    writer->endElement(); // w:rPr
+    if (!m_insidetable)
+    {
+        writer->endElement();    // w:rPr
+    }
 }
 
 void OdfTextReaderDocxBackend::endRun(OdfReaderDocxContext *docxContext)
@@ -363,9 +669,11 @@ void OdfTextReaderDocxBackend::endRun(OdfReaderDocxContext *docxContext)
     {
         writer = docxContext->m_commentsWriter;
     }
-    writer->endElement(); // w:r
-
-    if (m_writeComment && !m_insideComment)
+    if (!m_insidetable)
+    {
+        writer->endElement(); // w:r
+    }
+    if (m_writeComment && !m_insideComment && !m_insidetable)
     {
         writer->startElement("w:commentRangeEnd");
         writer->addAttribute("w:id", m_commentIndex);
