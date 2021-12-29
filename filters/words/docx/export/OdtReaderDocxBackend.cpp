a50e13e5fd71b1f183620b8c5327e61896688deb
zyq1569 <zyq1569@126.com>
2021-12-29 17:22:39 +0800

    Modified   filters/words/docx/export/OdtReaderDocxBackend.cpp

--- filters/words/docx/export/OdtReaderDocxBackend.cpp
+++ filters/words/docx/export/OdtReaderDocxBackend.cpp
@@ -77,8 +77,43 @@ void OdtReaderDocxBackend::elementOfficeBody(KoXmlStreamReader &reader, OdfReade
 
         // Start the document and add all necessary namespaces to it.
         writer->startElement("w:document");
+        ///----------------------------------------
+        //writer->addAttribute("xmlns:r", "http://schemas.openxmlformats.org/officeDocument/2006/relationships");
+        //writer->addAttribute("xmlns:w", "http://schemas.openxmlformats.org/wordprocessingml/2006/main");
+        ///openword add -------------------------------------ms2016----------------------------------------
+        writer->addAttribute("xmlns:wpc", "http://schemas.microsoft.com/office/word/2010/wordprocessingCanvas");
+        writer->addAttribute("xmlns:cx", "http://schemas.microsoft.com/office/drawing/2014/chartex");
+        writer->addAttribute("xmlns:cx1", "http://schemas.microsoft.com/office/drawing/2015/9/8/chartex");
+        writer->addAttribute("xmlns:cx2", "http://schemas.microsoft.com/office/drawing/2015/10/21/chartex");
+        writer->addAttribute("xmlns:cx3", "http://schemas.microsoft.com/office/drawing/2016/5/9/chartex");
+        writer->addAttribute("xmlns:cx4", "http://schemas.microsoft.com/office/drawing/2016/5/10/chartex");
+        writer->addAttribute("xmlns:cx5", "http://schemas.microsoft.com/office/drawing/2016/5/11/chartex");
+        writer->addAttribute("xmlns:cx6", "http://schemas.microsoft.com/office/drawing/2016/5/12/chartex");
+        writer->addAttribute("xmlns:cx7", "http://schemas.microsoft.com/office/drawing/2016/5/13/chartex");
+        writer->addAttribute("xmlns:cx8", "http://schemas.microsoft.com/office/drawing/2016/5/14/chartex");
+        writer->addAttribute("xmlns:mc", "http://schemas.openxmlformats.org/markup-compatibility/2006");
+        writer->addAttribute("xmlns:aink", "http://schemas.microsoft.com/office/drawing/2016/ink");
+        writer->addAttribute("xmlns:am3d", "http://schemas.microsoft.com/office/drawing/2017/model3d");
+        writer->addAttribute("xmlns:o", "urn:schemas-microsoft-com:office:office");
         writer->addAttribute("xmlns:r", "http://schemas.openxmlformats.org/officeDocument/2006/relationships");
+        writer->addAttribute("xmlns:m", "http://schemas.openxmlformats.org/officeDocument/2006/math");
+        writer->addAttribute("xmlns:v", "urn:schemas-microsoft-com:vml");
+        writer->addAttribute("xmlns:wp14", "http://schemas.microsoft.com/office/word/2010/wordprocessingDrawing");
+        writer->addAttribute("xmlns:wp", "http://schemas.openxmlformats.org/drawingml/2006/wordprocessingDrawing");
+        writer->addAttribute("xmlns:w10", "urn:schemas-microsoft-com:office:word");
         writer->addAttribute("xmlns:w", "http://schemas.openxmlformats.org/wordprocessingml/2006/main");
+        writer->addAttribute("xmlns:w14", "http://schemas.microsoft.com/office/word/2010/wordml");
+        writer->addAttribute("xmlns:w15", "http://schemas.microsoft.com/office/word/2012/wordml");
+        writer->addAttribute("xmlns:w16cex", "http://schemas.microsoft.com/office/word/2018/wordml/cex");
+        writer->addAttribute("xmlns:w16cid", "http://schemas.microsoft.com/office/word/2016/wordml/cid");
+        writer->addAttribute("xmlns:w16", "http://schemas.microsoft.com/office/word/2018/wordml");
+        writer->addAttribute("xmlns:w16sdtdh", "http://schemas.microsoft.com/office/word/2020/wordml/sdtdatahash");
+        writer->addAttribute("xmlns:w16se", "http://schemas.microsoft.com/office/word/2015/wordml/symex");
+        writer->addAttribute("xmlns:wpg", "http://schemas.microsoft.com/office/word/2010/wordprocessingGroup");
+        writer->addAttribute("xmlns:wpi", "http://schemas.microsoft.com/office/word/2010/wordprocessingInk");
+        writer->addAttribute("xmlns:wne", "http://schemas.microsoft.com/office/word/2006/wordml");
+        writer->addAttribute("xmlns:wps", "http://schemas.microsoft.com/office/word/2010/wordprocessingShape");
+        writer->addAttribute("mc:Ignorable", "w14 w15 w16se w16cid w16 w16cex w16sdtdh wp14");
 
         writer->startElement("w:body");
     }
