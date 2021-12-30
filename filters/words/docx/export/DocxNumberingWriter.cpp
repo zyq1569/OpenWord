4ab483a62b56d92ff8fcb90059b9bcd602b22abe
zyq1569 <zyq1569@126.com>
2021-12-30 14:52:08 +0800

    Added      filters/words/docx/export/DocxNumberingWriter.cpp Added      filters/words/docx/export/DocxNumberingWriter.h

---
+++ filters/words/docx/export/DocxNumberingWriter.cpp
@@ -0,0 +1,27 @@
+#include "DocxNumberingWriter.h"
+
+
+// Qt
+#include <QByteArray>
+#include <QBuffer>
+
+// Calligra
+#include <KoXmlWriter.h>
+#include <OdfReaderContext.h>
+#include <KoOdfStyleManager.h>
+#include <KoOdfStyle.h>
+#include <KoOdfStyleProperties.h>
+
+// This filter
+#include "DocxStyleHelper.h"
+
+
+DocxNumberingWriter::DocxNumberingWriter(OdfReaderContext *context)
+{
+
+}
+
+DocxNumberingWriter::~DocxNumberingWriter()
+{
+
+}
---
+++ filters/words/docx/export/DocxNumberingWriter.h
@@ -0,0 +1,29 @@
+#ifndef DOCXNUMBERING_H
+#define DOCXNUMBERING_H
+
+// Qt
+#include <QBuffer>
+
+class QByteArray;
+
+class KoXmlWriter;
+class OdfReaderContext;
+
+class DocxNumberingWriter
+{
+public:
+    DocxNumberingWriter(OdfReaderContext *context);
+    virtual ~DocxNumberingWriter();
+
+    void read();
+
+    QByteArray documentContent() const;
+
+private:
+    OdfReaderContext *m_readerContext;
+    QByteArray   m_documentContent; // m_documentWriter writes here;
+    KoXmlWriter *m_documentWriter;  // XML writer for the document contents
+    QBuffer      m_documentIO;      // IODevice for the XMl writer
+};
+
+#endif // DOCXNUMBERING_H
