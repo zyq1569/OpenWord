028281311876930d1b2b83313fe244d7471ec588
zyq1569 <zyq1569@126.com>
2022-01-23 12:57:47 +0800

    fine-tune

--- libs/version/CalligraVersionWrapper.cpp
+++ libs/version/CalligraVersionWrapper.cpp
@@ -22,7 +22,8 @@
 
 QString CalligraVersionWrapper::versionYear()
 {
-    return QLatin1Literal(CALLIGRA_YEAR);
+    //return QLatin1Literal(CALLIGRA_YEAR);
+     return QLatin1String(CALLIGRA_YEAR);
 }
 
 QString CalligraVersionWrapper::versionString(bool checkGit)
@@ -30,7 +31,8 @@ QString CalligraVersionWrapper::versionString(bool checkGit)
     QString calligraVersion(CALLIGRA_VERSION_STRING);
     QString version = calligraVersion;
 
-    if (checkGit) {
+    if (checkGit)
+    {
 #ifdef CALLIGRA_GIT_SHA1_STRING
         QString gitVersion(CALLIGRA_GIT_SHA1_STRING);
         version = QString("%1 (git %2)").arg(calligraVersion).arg(gitVersion).toLatin1();
